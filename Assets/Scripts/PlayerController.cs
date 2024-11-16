64bb74d1fb90f2027d64c59840da81b290ddae3b
jammytoaster <taylan2009@live.co.uk>
2024-11-16 16:39:57 +0000

    created playable character, added first person camera, added movement, added jumping, added sprinting, added crouching

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,179 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    private float speed;
+    public float walkSpeed;
+    public float sprintSpeed;
+
+
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header ("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
+
+    [Header("KeyBinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
+
+    [Header("Ground Check")]
+
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    Rigidbody rb;
+
+    public Transform orientation;
+
+    float xInput;
+    float yInput;
+
+    Vector3 moveDirection;
+
+    public MovementState state;
+    public enum MovementState{
+        walking,
+        sprinting,
+        crouching,
+        air
+    }
+
+
+    void Start(){
+         rb = GetComponent<Rigidbody>();
+         rb.freezeRotation = true;
+         readyToJump = true;
+
+         startYScale = transform.localScale.y;
+
+    }
+
+    void Update(){
+
+        // Raycast down to check ground
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+        StateHandler();
+
+        // Handle drag for when player is on the ground
+        if (grounded){
+            rb.linearDamping = groundDrag;
+        }
+        else{
+            rb.linearDamping = 0;
+        }
+
+    }
+
+    private void FixedUpdate(){
+        MovePlayer();
+    }
+
+    private void MyInput(){
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+
+
+        // When to jump
+        if(Input.GetKey(jumpKey) && readyToJump && grounded){
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(resetJump), jumpCooldown);
+        }
+
+        // Crouching
+
+        if(Input.GetKeyDown(crouchKey)) {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+
+        // Stop Crouching
+        if(Input.GetKeyUp(crouchKey)){
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
+
+    }
+
+    private void StateHandler(){
+
+        // Mode - Crouching
+        if (Input.GetKey(crouchKey)){
+            state = MovementState.crouching;
+            speed = crouchSpeed;
+        }
+
+        // Mode - Sprinting
+        if (grounded && Input.GetKey(sprintKey)){
+            state = MovementState.sprinting;
+            speed = sprintSpeed;
+        }
+
+        // Mode - Walking
+        else if (grounded){
+            state = MovementState.walking;
+            speed = walkSpeed;
+        }
+
+        // Mode - Air
+        else{
+            state = MovementState.air;
+        }
+    }
+
+    private void MovePlayer(){
+        // Calculate movement direction
+
+        moveDirection = orientation.forward * yInput + orientation.right * xInput;
+
+
+        if(grounded){
+            rb.AddForce(moveDirection.normalized * speed * 10f, ForceMode.Force);
+        }
+
+        else if(!grounded){
+            rb.AddForce(moveDirection.normalized * speed * 10f * airMultiplier, ForceMode.Force);
+        }
+    }
+
+    private void SpeedControl(){
+
+        Vector3 flatVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        // If you go faster than movement speed, find what velocity then apply
+        if(flatVelocity.magnitude > speed){
+            Vector3 limitedVelocity = flatVelocity.normalized * speed;
+            rb.linearVelocity = new Vector3(limitedVelocity.x, rb.linearVelocity.y, limitedVelocity.z);
+        }
+    }
+
+    private void Jump(){
+        // Resets y velocity
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void resetJump(){
+        readyToJump = true;
+    }
+
+}
